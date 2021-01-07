34f45611b7475ee70c9952aaebd7ab8134a983bf
Denys <quux@ukr.net>
2021-01-07 11:51:19 +0200

    step-3-initialize-plugin-with-your-publishable-key

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HyperTrack} from '@ionic-native/hyper-track';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  buttonClicked() {
+    console.log("Button Clicked")
+    console.log("Initializing HyperTrack")
+    HyperTrack.enableDebugLogging()
+    HyperTrack.initialize('YOUR-PUBLISHABLE-KEY-HERE')
+    .then( this.onSdkInstanceReceived )
+    .catch((err) => console.error("HyperTrack init failed with error " + err));  
+    
+  }
+
+   onSdkInstanceReceived(sdkInstance: HyperTrack) {
+    console.log("HyperTrack succesfully initialized");
+  }
 
 }
